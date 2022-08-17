47c3f9b304c4a43d1f5e3eec70f1877d8d74b176
Piyaphat Pinyo <remove158@gmail.com>
2022-08-17 22:04:25 +0700

    :bug: fix: showcase image and contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,18 +1,37 @@
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
 import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-	const router = useRouter();
-	const contacts = [
-		{ text: "Github", url: "https://github.com/remove158" },
+	const colSpan = [
+		"md:col-span-3",
+		"md:col-span-2",
+		"md:col-span-2",
+		"md:col-span-3",
+	];
+	const items = [
 		{
-			text: "Linkedin",
-			url: "https://th.linkedin.com/in/piyaphat-pinyo-446a74224",
+			text: "GITHUB",
+			url: "https://github.com/remove158",
+			img: "https://repository-images.githubusercontent.com/329529820/18c8b280-5748-11eb-9cb3-dac2176b97ec",
 		},
-		{ text: "Facebook", url: "https://www.fb.com/piyaphat.py" },
-		{ text: "Instagram", url: "https://www.instagram.com/plangton.n/" },
+		{
+			text: "LINKEDIN",
+			url: "https://th.linkedin.com/in/piyaphat-pinyo-446a74224",
+			img: "https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw",
+		},
+		{
+			text: "FACEBOOK",
+			url: "https://www.fb.com/piyaphat.py",
+			img: "https://www.thumbsup.in.th/wp-content/uploads/2020/03/fb-hero-image-001.jpeg",
+		},
+		{
+			text: "INSTAGRAM",
+			url: "https://www.instagram.com/plangton.n/",
+			img: "https://www.blognone.com/sites/default/files/externals/aa0e6da1a1e76da8965123c19184e582.png",
+		},
 	];
 	return (
 		<div className="bg-[#082424] h-full">
@@ -57,29 +76,31 @@ const Home: NextPage = () => {
 						</Link>
 					</motion.span>
 				</div>
-				<div className="flex-1 flex flex-col items-center justify-center text-white">
-					{contacts.map((contact, idx) => (
-						<motion.button
-							key={idx}
-							onClick={() => router.push(contact.url)}
-							className="my-4 py-2  rounded-md border  w-3/4"
-							initial={{ x: "-100vw" }}
-							animate={{ x: 0 }}
-							whileHover={{
-								scale: 1.1,
-								color: "#bef264",
-								borderColor: "#bef264",
-								transition: {
-									type: "spring",
-									stiffness: 300,
-									duration: 0.2,
-								},
-							}}
-							transition={{ delay: 0.2 * (idx + 1), type: "spring" }}
-						>
-							{contact.text}
-						</motion.button>
-					))}
+				<div className="flex-1 flex flex-col items-center text-white px-4 group">
+					<div className="grid grid-cols-1 md:grid-cols-5 w-full gap-4">
+						{items.map((item, idx) => (
+							<motion.a
+								href={item.url}
+								key={idx}
+								initial={{ x: "-100vw" }}
+								animate={{ x: 0 }}
+								transition={{ delay: 0.3 * (idx + 1), type: "spring" }}
+								className={`card ${colSpan[idx % 4]}`}
+								style={{ backgroundImage: `url("${item.img}")` }}
+							>
+								<p className="mb-5 max-w-xl font-extrabold tracking-tight leading-tight text-white">
+									{item.text}
+								</p>
+
+								<button
+									type="button"
+									className="inline-flex items-center px-2.5 py-1.5 text-xs font-medium text-center text-white border border-white rounded-lg hover:bg-white hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-700"
+								>
+									CONTACT ME
+								</button>
+							</motion.a>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/pages/showcase.tsx
+++ src/pages/showcase.tsx
@@ -5,18 +5,31 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-	const router = useRouter();
-	const contacts = [
+	const colSpan = [
+		"md:col-span-3",
+		"md:col-span-2",
+		"md:col-span-2",
+		"md:col-span-3",
+	];
+	const items = [
 		{
-			text: "We Share",
+			text: "WE SHARE",
 			url: "https://bill.piyaphat.com",
+			img: "/weshare.png",
 			description:
 				"เว็บไซต์สำหรับหารบิลกับเพื่อนแบบ realtime เราช่วยให้คุณสามารถจัดแจงค่าใช้จ่ายได้อย่างง่ายดาย",
 		},
 		{
-			text: "OSM Craft",
+			text: "OSM CRAFT",
 			url: "https://minecraft.piyaphat.com",
+			img: "/osmcraft.png",
 			description: "จำลอง minecraft สร้างโดยใช้ Three.js พร้อมระบบฟิสิกส์",
+		},
+		{
+			text: "DISCORD BOT",
+			url: "https://discord.com/api/oauth2/authorize?client_id=805856620026265610&permissions=8&scope=applications.commands%20bot",
+			img: "/discord.png",
+			description: "discord  bot ฟังเพลงกับเพื่อน",
 		},
 	];
 	return (
@@ -62,42 +75,33 @@ const Home: NextPage = () => {
 						</Link>
 					</motion.span>
 				</div>
-				<div className="flex-1 flex flex-col items-center  text-white">
-					{contacts.map((contact, idx) => (
-						<motion.div
-							key={idx}
-							onClick={() => router.push(contact.url)}
-							className="my-4 p-4  rounded-md border  w-3/4 flex flex-col"
-							initial={{ x: "-100vw" }}
-							animate={{ x: 0 }}
-							whileHover={{
-								scale: 1.1,
-								color: "#bef264",
-								borderColor: "#bef264",
-								transition: {
-									type: "spring",
-									stiffness: 300,
-									duration: 0.2,
-								},
-							}}
-							transition={{ delay: 0.2 * (idx + 1), type: "spring" }}
-						>
-							<motion.h1
-								className="text-xl"
-								initial={{ x: -100 }}
+				<div className="flex-1 flex flex-col items-center text-white px-4 group">
+					<div className="grid grid-cols-1 md:grid-cols-5 w-full gap-4">
+						{items.map((item, idx) => (
+							<motion.a
+								href={item.url}
+								key={idx}
+								initial={{ x: "-100vw" }}
 								animate={{ x: 0 }}
+								transition={{ delay: 0.3 * (idx + 1), type: "spring" }}
+								className={`card ${colSpan[idx % 4]}`}
+								style={{ backgroundImage: `url("${item.img}")` }}
 							>
-								{contact.text}
-							</motion.h1>
-							<motion.span
-								className="font-thin"
-								initial={{ x: -100 }}
-								animate={{ x: 0 }}
-							>
-								{contact.description}
-							</motion.span>
-						</motion.div>
-					))}
+								<p className="max-w-xl font-extrabold text-lg tracking-tight leading-tight text-white">
+									{item.text}
+								</p>
+								<p className="mb-5 max-w-xl  tracking-tight leading-tight text-white">
+									{item.description}
+								</p>
+								<button
+									type="button"
+									className="inline-flex items-center px-2.5 py-1.5 text-xs font-medium text-center text-white border border-white rounded-lg hover:bg-white hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-700"
+								>
+									View Site
+								</button>
+							</motion.a>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
