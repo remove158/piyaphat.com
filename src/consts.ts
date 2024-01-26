b5c866e9ff8464dfeca35fb5eae05b4306765e74
remove158 <remove158@gmail.com>
2024-01-27 02:21:41 +0700

    add: hooks

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Piyaphat.com';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = `Hello, My name is Piyaphat Pinyo. I'm a progamer.`;
 export const GITHUB_LINK = 'https://www.github.com/remove158'
 export const GITHUB_REPO_LINK = 'https://www.github.com/remove158/piyaphat.com'
