84be8c76c5e81954e0aa9d4b9c43f9f37410bffa
remove158 <remove158@gmail.com>
2024-01-27 13:39:44 +0700

    feat: authors

--- src/content/blog/first-post.md
+++ src/content/blog/first-post.md
@@ -1,5 +1,6 @@
 ---
-title: 'First post'
+title: 'First postaa'
+author: 'piyaphat' 
 description: 'Lorem ipsum dolor sit amet'
 pubDate: 'Jul 08 2022'
 heroImage: '/blog-placeholder-1.jpg'
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,26 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+const blog = defineCollection({
+  type: 'content',
   schema: z.object({
 	title: z.string(),
 	description: z.string(),
-	// Transform string to Date object
+	author: reference('authors'),
 	pubDate: z.coerce.date(),
 	updatedDate: z.coerce.date().optional(),
 	heroImage: z.string().optional(),
 }),
 });
 
+const authors  = defineCollection({
+	type: 'data',
+	schema: z.object({
+		name: z.string(),
+		git: z.string().url()
+	}),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
-export const collections = {
-  'blog': blogCollection,
-};
+export const collections = {  blog, authors };
